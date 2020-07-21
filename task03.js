7e955e0c6a3043fcc7d0ff9e731a9db11631aa12
sergey.ar10n@gmail.com <31yjdsq07gfhjkm82>
2020-07-21 20:03:40 +0300

    finished task03.js

--- task03.js
+++ task03.js
@@ -1,27 +1,53 @@
-let websocket;
-const sendButton = document.querySelector(".send_button");
-const sendGeo = document.querySelector(".send_geo");
-const output = document.getElementById("output");
+const sendButton = document.querySelector(".task-03-sndBtn");
+const sendGeo = document.querySelector(".task-03-sndGeo");
+const chat = document.querySelector(".task-03-chat");
 
-function writeToScreen(message) {
-    let pre = document.createElement("p");
-    pre.style.wordWrap = "word-wrap";
+function writeToScreen(message, textPosition, marginValue) {
+    let pre = document.createElement("div");
+    pre.style.wordWrap = "break-word";
     pre.innerHTML = message;
-    output.before(pre);
+    pre.style.maxWidth = "300px";
+    pre.style.padding = "10px";
+    pre.style.textAlign = textPosition;
+    pre.style.border = "solid 2px lightblue";
+    pre.style.borderRadius = "1px";
+    if (marginValue === "left") {
+        pre.style.marginLeft = "300px";
+        pre.style.marginRight = "10px";
+    } else {
+        pre.style.marginLeft = "10px";
+        pre.style.marginRight = "300px";
+    }
+    chat.appendChild(pre);
 }
 
 sendButton.addEventListener("click", () => {
     let webSocket = new WebSocket("wss://echo.websocket.org/");
-    let inputText = document.getElementById("input_text").value;
-    writeToScreen(
-        '<span style="color: red;">' + inputText + '</span>'
-    );
+    let inputText = document.querySelector("input").value;
+    writeToScreen(inputText, "right", "left");
     webSocket.onopen = () => {
         webSocket.send(inputText);
     }
-    webSocket.onmessage = function (evt) {
-        writeToScreen(
-            '<span style="color: blue;">' + evt.data + '</span><br>'
-        )
+    webSocket.onmessage = async function (evt) {
+        writeToScreen(evt.data, "left", "right");
+        await webSocket.close();
+    }
+})
+
+sendGeo.addEventListener("click", () => {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position => {
+            const { coords } = position;
+            console.log(coords)
+            let geoString = "https://www.openstreetmap.org/#map=13/" + coords.latitude + "/" + coords.longitude;
+            let webSocket = new WebSocket("wss://echo.websocket.org/");
+            writeToScreen(geoString, "right", "left");
+            webSocket.onopen = async () => {
+                webSocket.send(geoString);
+                await webSocket.close();
+            }
+        }))
+    } else {
+        alert("Включите геолокацию!")
     }
 })
